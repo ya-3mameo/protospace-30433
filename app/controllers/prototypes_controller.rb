3eb2932f61bfd8bb6941317065cf3836548fbf04
ya-3mameo <nao51.gren@gmail.com>
2020-10-13 18:46:48 +0900

    sixth commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: :root_path
-  before_action :move_to_index,  only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
 
   def index
